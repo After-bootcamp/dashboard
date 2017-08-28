5fbf66b9a02e125ad2f15068a181d0b481248d4a
Todd Gerdy <toddgerdy@Todds-iMac.local>
2017-08-27 22:34:23 -0400

    I have a rough form running

--- dashboard/src/Main.js
+++ dashboard/src/Main.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
+import CheckIn from "./CheckIn"
+
 class Main extends Component {
   render() {
     return (
       <div className="main_section">
         <h2>DASHBOARD</h2>
         <h3 id="check_in_button">Check In (if it is a new day)</h3>
+        <CheckIn />
       </div>
     )
   }
