5e85a62b45b446afbb8904eb701dd3d5b6236be5
Todd Gerdy <toddgerdy@Todds-iMac.local>
2017-08-28 23:28:51 -0400

    need to sleep but also need to finish up my menu.

--- dashboard/src/Header.js
+++ dashboard/src/Header.js
@@ -3,9 +3,23 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div className="header_section">
-        <div id="main_logo">After Bootcamp</div>
-        <div >menu</div>
+      <div className="container">
+        <div className="navbar fixed-top navbar-light" >
+          <a className="navbar-brand" href="#">After Bootcamp</a>
+          <div className="pos-f-t">
+            <div className="collapse" id="navbarToggleExternalContent">
+              <div className="p-4">
+                <h4 className="text-white">Main Menu</h4>
+
+              </div>
+            </div>
+          </div>
+          <nav className="navbar navbar-light">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          </nav>
+        </div>
       </div>
     )
   }
