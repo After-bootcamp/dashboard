aa958f47b2b9998a22308bb04051a3c3385cf596
JoshSummerTop <jwinterbotham@gmail.com>
2017-08-31 19:54:35 -0700

    update

--- dashboard/src/CheckIn.js
+++ dashboard/src/CheckIn.js
@@ -1,35 +1,76 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Modal } from 'react-bootstrap';
+import Rating from './Rating';
 
 class CheckIn extends Component {
   constructor (props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {
+      showModal: false,
+      question1 : ' ',
+      question2 : ' ',
+      question3 : ' ',
+      question4 : ' ',
+      question5 : ' ',
+      question6 : ' ',
+    }
 
-    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.open = this.open.bind(this)
+    this.close = this.close.bind(this)
+
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  close() {
+    this.setState({ showModal: false })
+  }
+
+  open() {
+    this.setState({ showModal: true })
   }
 
   handleSubmit(event) {
-    alert('A name was submited: '+ this.state.value);
-    console.log(this.state.value);
-    event.preventDefault();
+    event.preventDefault()
+    console.log('You have selected:', this.state.question1)
+  }
+
+  handleAddRating(question, rating) {
+    // this.setState({
+    //   question1: rating,
+    //   question2: rating,
+    //   question3: rating,
+    //   question4: rating,
+    //   question5: rating,
+    //   question6: rating
+    // })
+    console.log(question)
   }
 
   render() {
     return (
-      <div className="check_in_section">
-        <h1>CheckIn modal to be added here.</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange}/>
-          </label>
-          <input type="submit" value="Submit"/>
-        </form>
+      <div>
+        <h3
+          className="btn btn-success"
+          onClick={this.open}
+          >
+            Check In (if it is a new day)
+        </h3>
+        <Modal className="check_in_section" show={this.state.showModal} onHide={this.close}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <form onSubmit={this.handleSubmit} action="">
+              <Rating question={'question1'} addRating={this.handleAddRating.bind(this)}/>
+              <Rating question={'question2'} addRating={this.handleAddRating.bind(this)}/>
+              <Rating question={'question3'} addRating={this.handleAddRating.bind(this)}/>
+              <Rating question={'question4'} addRating={this.handleAddRating.bind(this)}/>
+              <Rating question={'question5'} addRating={this.handleAddRating.bind(this)}/>
+              <Rating question={'question6'} addRating={this.handleAddRating.bind(this)}/>
+              <button>Submit</button>
+            </form>
+          </Modal.Body>
+        </Modal>
       </div>
     )
   }
---
+++ dashboard/src/Rating.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { FormGroup, Radio } from 'react-bootstrap';
+
+class Rating extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedRating: '1'
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({selectedRating: event.target.value
+
+
+    },function() {
+      this.props.addRating(this.props.question, this.state.selectedRating)
+    })
+  }
+
+  render() {
+    return (
+      <FormGroup>
+        <Radio
+          value="1"
+          name="radioGroup"
+          checked={this.state.selectedRating === '1'}
+          onChange={this.handleChange}
+          inline>
+          1
+        </Radio>
+        <Radio
+          value="2"
+          name="radioGroup"
+          checked={this.state.selectedRating === '2'}
+          onChange={this.handleChange}
+          inline>
+          2
+        </Radio>
+        <Radio
+          value="3"
+          name="radioGroup"
+          checked={this.state.selectedRating === '3'}
+          onChange={this.handleChange}
+          inline>
+          3
+        </Radio>
+        <Radio
+          value="4"
+          name="radioGroup"
+          checked={this.state.selectedRating === '4'}
+          onChange={this.handleChange}
+          inline>
+          4
+        </Radio>
+        <Radio
+          value="5"
+          name="radioGroup"
+          checked={this.state.selectedRating === '5'}
+          onChange={this.handleChange}
+          inline>
+          5
+        </Radio>
+          Question 1
+      </FormGroup>
+    )
+  }
+}
+
+export default Rating;
