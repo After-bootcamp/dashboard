f33afd693c0e4e2b0f670221a340b4e562c94e10
Todd Gerdy <toadimous@gmail.com>
2017-12-22 23:07:58 -0500

    fixed an error I was having with the chart.js page

--- src/Charts.js
+++ src/Charts.js
@@ -1,18 +1,18 @@
 import React from "react";
 
-const Charts = (props) => 
-  return (
-    <div>
-      <h2>Charts(some kind of progress thingy)</h2>
-      <h3>Code Challenges: {props.currentData.selectedOption1}</h3>
-      <h3>Job Search: {props.currentData.selectedOption2}</h3>
-      <h3>Networking: {props.currentData.selectedOption3}</h3>
-      <h3>Algorithms: {props.currentData.selectedOption4}</h3>
-      <h3>Coding Playground: {props.currentData.selectedOption5}</h3>
-      <h3>Community Forum: {props.currentData.selectedOption6}</h3>
-      <h3>Available Time: {props.currentData.selectedHours}</h3>
-    </div>
-  );
+const Charts = (props) => {
+    return (
+      <div>
+        <h2>Charts(some kind of progress thingy)</h2>
+        <h3>Code Challenges: {props.currentData.selectedOption1}</h3>
+        <h3>Job Search: {props.currentData.selectedOption2}</h3>
+        <h3>Networking: {props.currentData.selectedOption3}</h3>
+        <h3>Algorithms: {props.currentData.selectedOption4}</h3>
+        <h3>Coding Playground: {props.currentData.selectedOption5}</h3>
+        <h3>Community Forum: {props.currentData.selectedOption6}</h3>
+        <h3>Available Time: {props.currentData.selectedHours}</h3>
+      </div>
+    )
 };
 
 
