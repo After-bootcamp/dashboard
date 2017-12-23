1786b97f300e6c7d2fe60457eb14da880ba8c7e0
Todd Gerdy <toadimous@gmail.com>
2017-12-23 01:48:56 -0500

    About Us page has been started

--- src/Stats.js
+++ src/Stats.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 import CheckIn from "./CheckIn";
 import Charts from "./Charts";
 
-class Main extends Component {
+class Stats extends Component {
   constructor(props) {
     super();
-    this.state ={
-      todaysData: props.todaysData,
+    this.state = {
+      // todaysData: props.todaysData,
       currentData: {
         showModal: false,
         selectedOption1: '0',
@@ -18,34 +18,27 @@ class Main extends Component {
         selectedOption6: '0',
         selectedHours: '0'
       }
-
     }
   }
-
   showAnswers(answers) {
     this.setState({currentData : answers}, console.log(this.state))
     console.log('the value of answers: ', answers);
   }
 
   render() {
-    // const dataList = this.props.todaysData.map(item => {
-    //   return (
-    //     <p> {item.test} </p>
-    //   )
-    // })
     return (
       <div className="main_section">
-      <h2>DASHBOARD</h2>
-      <CheckIn
-        todaysData={this.props.todaysData}
-        showAnswers={this.showAnswers.bind(this)}
-      />
-      <Charts
-        currentData={this.state.currentData}
-      />
+        <h2>DASHBOARD</h2>
+        <CheckIn
+          todaysData={this.props.todaysData}
+          showAnswers={this.showAnswers.bind(this)}
+        />
+        <Charts
+          currentData={this.state.currentData}
+        />
       </div>
     )
   }
 }
 
-export default Main;
+export default Stats;
